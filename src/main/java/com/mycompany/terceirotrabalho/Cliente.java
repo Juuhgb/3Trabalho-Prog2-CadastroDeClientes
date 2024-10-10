d82e78c2f7148c0d7a0472102811f7a4c973550e
Juliana <juliana.g.bello@gmail.com>
2024-10-09 22:59:52 -0300

    first commit

---
+++ src/main/java/com/mycompany/terceirotrabalho/Cliente.java
@@ -0,0 +1,8 @@
+package com.mycompany.terceirotrabalho;
+
+public class Cliente extands Endereco{
+    private String nome;
+    private Integer codigo;
+    private Endereco endereco;
+    private String telefone;
+}
